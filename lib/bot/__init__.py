1c849a5fa84fcbc859116a4657f902b1eb500e30
Phillip Nguyen <73822780+NophysWaffles@users.noreply.github.com>
2021-07-06 16:13:40 -0700

    update for July 6
    need to debug sch issue

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,21 +1,33 @@
+from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands.errors import CommandNotFound
+from discord.ext.commands import CommandNotFound
+from ..db import db
 
 PREFIX= "+"
-OWNER_IDS = [850249588857503744]
+OWNER_IDS = [204529741225525248]
 
 class Bot(BotBase):
     def __init__(self):
+        
         self.PREFIX = PREFIX
         self.ready = False 
         self.guild = None
         self.scheduler = AsyncIOScheduler()
-        super().__init__(command_prefix = PREFIX, owner_ids=OWNER_IDS)
+        
+        db.autosave()
+        super().__init__(
+            command_prefix = PREFIX,
+             owner_ids=OWNER_IDS,
+             intents=Intents.all(),
+             )
 
     def run(self, version):
         self.VERSION = version
 
-        with open("./lib/bot/token", "r", encoding="utf=8") as tf:
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
 
         print("running")
@@ -26,17 +38,50 @@ class Bot(BotBase):
 
     async def on_disconnect(self):
         print("bot disconnected")
+
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_commmand_error":
+            await args[0].send("ruh roh.....")
+        
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+
+        elif hasattr(exc, "original"):
+            raise exc.original
     
+        else:   
+            raise exc
+            
     async def on_ready(self):
         if not self.ready:
             self.ready = True
             self.guild = self.get_guild(360940298320216067)
+            self.schedular.start()
+            
+
+            channel = self.get_channel(851864375479762994)
+            await channel.send("bot online")
+
+            # embed = Embed(title="online", description="bot is online", color=0xFF0001)
+            # embed.add_field(name="NAME", value="Value", inline=True)
+            # fields = [("Name", "Value", True),
+            #         ("another field", "idk what this does", True),
+            #         ("none-inline field", "appear on its own row", False)]
+            # for name, value, inline in fields:
+            #     embed.add_field(name=name, value=value, inline=inline)
+                
+            
+            await channel.send(embed=embed)
+
             print("ready")
 
-        else:
+        else:        
+
             print("reconnected")
     
-
     async def on_message(self, message):
         pass
 
